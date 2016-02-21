275010e303f2adbc5c4e0bba93f964e058f33cd2
YOUR NAME <YOUR@EMAIL.com>
2016-02-21 07:02:55 +0200

    -local

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,11 @@ class ApplicationController < ActionController::Base
 
  	protected
 
-	 def authorize
-		unless User.find_by(id: session[:user_id])
-		redirect_to login_url, notice: "Пожалуйста, пройдите авторизацию"
-	end
-end
+	  def authorize
+		  unless User.find_by(id: session[:user_id])
+		    redirect_to login_url, notice: "Пожалуйста, пройдите авторизацию"
+	    end
+    end
+  
+  
 end
