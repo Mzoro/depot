32acf7b4a84d98ada7e61937bee4143bfc64fc64
YOUR NAME <YOUR@EMAIL.com>
2016-02-22 23:21:12 +0200

    fix wrong messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,12 +10,12 @@ class SessionsController < ApplicationController
 		  session[:user_id] = user.id
 			redirect_to admin_url
 		else
-			redirect_to login_url, alert: "Неверная комбинация имени и пароля"
+			redirect_to login_url, alert: "Invalid user/password combination"
 		end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to store_url, notice: "Сеанс работы завершен"
+    redirect_to store_url, notice: "Logged out"
   end
 end
