48d18120822b99606e072942e4a90b2ace5dc565
YOUR NAME <YOUR@EMAIL.com>
2016-02-24 03:24:07 +0200

    add test

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -26,7 +26,6 @@ class ProductsControllerTest < ActionController::TestCase
     assert_difference('Product.count') do
       post :create, product: @update   
     end
-
     assert_redirected_to product_path(assigns(:product))
   end
 
@@ -45,11 +44,25 @@ class ProductsControllerTest < ActionController::TestCase
     assert_redirected_to product_path(assigns(:product))
   end
 
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete :destroy, id: products(:ruby)
+    end
+
+    assert_redirected_to products_path
+  end
+
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete :destroy, id: @product
     end
 
     assert_redirected_to products_path
+  end
+
+  test "should require login" do
+    logout
+    get :index
+    assert_redirected_to login_path
   end
 end
